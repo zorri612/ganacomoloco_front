4a9726d26e04413d7367525cac0bdee5e277e41e
Nicolas <zorrillanico200312@gmail.com>
2024-10-27 15:08:24 -0500

    Redeploying the project

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -43,7 +43,7 @@ export default function Login({ callback }) {
   return (
     <div className="login-container">
       <div className="login-form-container">
-        <h2>Iniciar sesión</h2>
+        <h2>Iniciar sesiónnnnn</h2>
         <form className="login-form" onSubmit={validateUser}>
           <div className="form-group">
             <label htmlFor="email-address">Correo electrónico</label>        
